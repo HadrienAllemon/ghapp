052cde8b6accde328fd2a26001680d3d189ef8c8
Hadrien Allemon <hadrienallemon@gmail.com>
2018-09-17 13:58:26 +0200

    inital commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        I am the homepage
+		<a href='html2.html'> GO to the second page </a>
+      </div>
+    );
+  }
+}
+
+export default App;
